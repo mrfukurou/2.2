f6e851b68c15313749d9eec2bf8d6a9e99031f55
fukur <fukur@LAPTOP-7EQMGM6H>
2020-05-07 20:49:28 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                Console.Write("Введите пол\nДля женских имен - ж\nДля мужских имен - м\n");
+                string n = Convert.ToString(Console.ReadLine());
+                if (n == "ж")
+                    Console.WriteLine("Женские имена: Мария, Виолетта, Юлия, Алина, Ирина");
+                else if (n == "м") Console.WriteLine("Мужские имена: Денис, Михаил, Никита, Богдан, Дмитрий");
+                else Console.WriteLine("Некорректный ввод данных");
+        }
+    }
+}
